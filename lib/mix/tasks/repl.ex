56e7338ef7ac4c9ebb804da33f86fec01e0de44d
Christian Bäuerlein <fabrik42@gmail.com>
2017-01-08 17:08:30 +0100

    adds welcome message to the repl

--- lib/mix/tasks/repl.ex
+++ lib/mix/tasks/repl.ex
@@ -2,6 +2,10 @@ defmodule Mix.Tasks.Repl do
   use Mix.Task
 
   def run(_) do
+    user = System.cmd("whoami", []) |> elem(0) |> String.rstrip
+
+    IO.puts("Hello #{user}! This is the Monkey programming language!")
+    IO.puts("Feel free to type in commands")
     Monkey.Repl.loop()
   end
 end
